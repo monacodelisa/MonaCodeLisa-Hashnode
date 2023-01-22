635fb691f79983cf49eefd6f25e4b36193d0f927
Esther White <esteewhite@hotmail.com>
2023-01-22 22:53:17 +0200

    adding stuff

--- projects-widget/script.js
+++ projects-widget/script.js
@@ -1,10 +1,14 @@
 const projectsContainer = document.querySelector(".projects-container");
 const projectsGallery = document.querySelector(".projects-gallery");
 const projectsMain = document.querySelector(".projects-main");
+const mainImgWrapper = document.querySelector(".main-image-wrapper");
 const mainImg = document.querySelector(".main-img");
-const mainLink = document.createElement("div");
+const mainImgCaption = document.querySelector(".main-img-caption");
 const threeGridImg = document.querySelectorAll(".three-grid");
 const threeGridLinks = document.querySelectorAll(".three-grid-links");
+
+const headings = document.querySelectorAll("h1");
+const links = document.querySelectorAll("a");
 
 let link = document.createElement("a");
 link.href = "https://www.example.com";
@@ -18,21 +22,27 @@ projectsGallery.setAttribute("style", "display: flex; justify-content: center; a
 
 // Main Image
 
-mainImg.style.cssText = "width: 50%; margin: auto; border-radius: 1rem; position: relative"
+mainImgWrapper.style.cssText = "width: 45vw; min-height: 55vh; margin: auto;position: relative;"
 
-mainLink.classList.add("main-link");
+mainImg.style.cssText = "width: 100%; margin-bottom: 2rem; border-radius: 1rem;"
 
-mainImg.addEventListener("mouseover", function() {
-	mainImg.appendChild(mainLink);
-	mainLink.style.cssText = "width: 5rem; height: 5rem; background-color: grey; color: white; position: absolute"
-	mainLink.innerText = "Hello";
+mainImgWrapper.addEventListener("mouseover", function() {
+    mainImgCaption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer"
 });
 
-mainImg.addEventListener("mouseout", function() {
-    mainImg.removeChild(mainLink);
+mainImgWrapper.addEventListener("mouseout", function() {
+    mainImgCaption.style.cssText = "display: none" 
 });
 
 
+//General Styles
+headings.forEach(heading => {
+	heading.style.cssText = "margin: 0; font-family: 'Ubuntu', sans-serif;"
+})
+
+links.forEach(link => {
+	link.style.cssText = "color: white"
+})
 // Gallery Images
 
 threeGridLinks.forEach(link => {
@@ -42,5 +52,6 @@ threeGridLinks.forEach(link => {
 threeGridImg.forEach(image => {
 	image.style.cssText = "width: 100%; margin: 0 1rem; border-radius: 1rem;"
 })
+
 
 // social.style.cssText = "margin-bottom: 1.5rem;"
