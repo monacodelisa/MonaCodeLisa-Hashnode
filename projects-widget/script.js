bbf3a59b5d6c7cad05340cf3f5d40837a1532dca
Esther White <esteewhite@hotmail.com>
2023-01-22 23:28:02 +0200

    stuff

--- projects-widget/script.js
+++ projects-widget/script.js
@@ -4,8 +4,9 @@ const projectsMain = document.querySelector(".projects-main");
 const mainImgWrapper = document.querySelector(".main-image-wrapper");
 const mainImg = document.querySelector(".main-img");
 const mainImgCaption = document.querySelector(".main-img-caption");
+const threeGridWrapper = document.querySelectorAll(".three-grid-img-wrapper");
 const threeGridImg = document.querySelectorAll(".three-grid");
-const threeGridLinks = document.querySelectorAll(".three-grid-links");
+const threeGridImgCaption = document.querySelectorAll(".three-grid-img-caption");
 
 const headings = document.querySelectorAll("h1");
 const links = document.querySelectorAll("a");
@@ -26,6 +27,8 @@ mainImgWrapper.style.cssText = "width: 45vw; min-height: 55vh; margin: auto;posi
 
 mainImg.style.cssText = "width: 100%; margin-bottom: 2rem; border-radius: 1rem;"
 
+mainImgCaption.style.cssText = "display: none"
+
 mainImgWrapper.addEventListener("mouseover", function() {
     mainImgCaption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer"
 });
@@ -35,23 +38,53 @@ mainImgWrapper.addEventListener("mouseout", function() {
 });
 
 
-//General Styles
-headings.forEach(heading => {
-	heading.style.cssText = "margin: 0; font-family: 'Ubuntu', sans-serif;"
-})
+// Gallery Grid 
 
-links.forEach(link => {
-	link.style.cssText = "color: white"
-})
-// Gallery Images
-
-threeGridLinks.forEach(link => {
-	link.style.cssText = "margin: 0 1rem;"
+threeGridWrapper.forEach(wrapper => {
+	wrapper.style.cssText = "margin: 0 1rem;"
 })
 
 threeGridImg.forEach(image => {
-	image.style.cssText = "width: 100%; margin: 0 1rem; border-radius: 1rem;"
+	image.style.cssText = "width: 100%; border-radius: 1rem;"
 })
 
+// threeGridImgCaption.forEach(caption => {
+//     caption.style.cssText = "display: none"
+// })
 
-// social.style.cssText = "margin-bottom: 1.5rem;"
+
+// threeGridWrapper.addEventListener("mouseover", function() {
+//     threeGridImgCaption.forEach(caption => {
+//         caption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;"
+//     });
+// });
+
+threeGridImgCaption.forEach( image => {
+    image.style.cssText = "display: none;"
+})
+
+threeGridWrapper.forEach( wrapper => {
+    wrapper.addEventListener("mouseover", function() {
+        threeGridImgCaption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;"
+    });
+})
+
+threeGridWrapper.addEventListener("mouseout", function() {
+    threeGridImgCaption.forEach(caption => {
+        caption.style.cssText = "display: none;"
+    });
+});
+
+
+//General Styles
+headings.forEach(heading => {
+	heading.style.cssText = "margin: 0; font-family: 'Ubuntu', sans-serif;"
+});
+
+links.forEach(link => {
+	link.style.cssText = "color: white;"
+});
+
+
+
+
