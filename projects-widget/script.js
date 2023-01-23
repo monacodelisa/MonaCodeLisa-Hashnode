ceec89e1a8b5d7166ea36b779231423dd46a788e
Esther White <esteewhite@hotmail.com>
2023-01-23 20:38:35 +0200

    slides

--- projects-widget/script.js
+++ projects-widget/script.js
@@ -1,6 +1,5 @@
 const projectsContainer = document.querySelector(".projects-container");
 const projectsGallery = document.querySelector(".projects-gallery");
-// const carousel = document.querySelector(".carousel");
 const mainImgWrapper = document.querySelector(".main-image-wrapper");
 const mainImgCaption = document.querySelector(".main-img-caption");
 const threeGridWrapper = document.querySelectorAll(".three-grid-img-wrapper");
@@ -36,11 +35,6 @@ paragraphs.forEach(p => {
 });
 
 
-// listItem.forEach(li => {
-// 	li.style.cssText = "text-align: left"
-// });
-
-
 // Carousel
 const track = document.querySelector(".carousel__track");
 const slides = Array.from(track.children);
@@ -50,11 +44,53 @@ const dotsNav = document.querySelector(".carousel__nav");
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
-console.log(slideWidth)
-// When I click left, move slides to the left
-// When I click right, move slides to the right
-// When I click the nav indicators move to corresponding slide
 
+// arrange the slides nex to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = `translateX(-${targetSlide.style.left})`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide'); 
+}
+// When I click left, move slides to the left
+prevBtn.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+})
+
+// When I click right, move slides to the right
+nextBtn.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+})
+
+
+// When I click the nav indicators move to corresponding slide
+dotsNav.addEventListener('click', e => {
+    // what indicator was clicked on
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+})
 
 // Main Image
 
@@ -92,7 +128,7 @@ threeGridImgCaption.forEach(caption => {
 threeGridWrapper.forEach((wrapper, index) => {
     wrapper.addEventListener("mouseover", function() {
         let imgCaption = threeGridImgCaption[index];
-        imgCaption.style.cssText = "position: absolute; top: 80px; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;";
+        imgCaption.style.cssText = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border-radius: 1rem; cursor: pointer;";
     });
 });
 
