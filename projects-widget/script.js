9800246d6cf5094f23ff2144fd3c9e2a39f7e05c
Esther White <esteewhite@hotmail.com>
2023-01-23 06:25:59 +0200

    projects gallery

--- projects-widget/script.js
+++ projects-widget/script.js
@@ -7,7 +7,6 @@ const mainImgCaption = document.querySelector(".main-img-caption");
 const threeGridWrapper = document.querySelectorAll(".three-grid-img-wrapper");
 const threeGridImg = document.querySelectorAll(".three-grid");
 const threeGridImgCaption = document.querySelectorAll(".three-grid-img-caption");
-
 const headings = document.querySelectorAll("h1");
 const links = document.querySelectorAll("a");
 
@@ -19,7 +18,7 @@ projectsContainer.setAttribute("style", "display: flex; flex-direction: column; 
 
 projectsMain.setAttribute("style", "display: flex; justify-content: center; align-items: center;")
 
-projectsGallery.setAttribute("style", "display: flex; justify-content: center; align-items: center;")
+
 
 // Main Image
 
@@ -40,38 +39,32 @@ mainImgWrapper.addEventListener("mouseout", function() {
 
 // Gallery Grid 
 
+projectsGallery.setAttribute("style", "display: flex; flex-wrap: wrap; justify-content: center; align-items: center;")
+
 threeGridWrapper.forEach(wrapper => {
-	wrapper.style.cssText = "margin: 0 1rem;"
+	wrapper.style.cssText = "position: relative; width: 30%; margin: 1rem;"
 })
 
 threeGridImg.forEach(image => {
 	image.style.cssText = "width: 100%; border-radius: 1rem;"
 })
 
-// threeGridImgCaption.forEach(caption => {
-//     caption.style.cssText = "display: none"
-// })
-
-
-// threeGridWrapper.addEventListener("mouseover", function() {
-//     threeGridImgCaption.forEach(caption => {
-//         caption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;"
-//     });
-// });
-
-threeGridImgCaption.forEach( image => {
-    image.style.cssText = "display: none;"
+threeGridImgCaption.forEach(caption => {
+	caption.style.cssText = "display: none"
 })
 
-threeGridWrapper.forEach( wrapper => {
+threeGridWrapper.forEach((wrapper, index) => {
     wrapper.addEventListener("mouseover", function() {
-        threeGridImgCaption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;"
+        let imgCaption = threeGridImgCaption[index];
+        imgCaption.style.cssText = "position: absolute; top: 80px; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer;";
     });
-})
+});
 
-threeGridWrapper.addEventListener("mouseout", function() {
-    threeGridImgCaption.forEach(caption => {
-        caption.style.cssText = "display: none;"
+
+threeGridWrapper.forEach((wrapper, index) => {
+    wrapper.addEventListener("mouseout", function() {
+        let imgCaption = threeGridImgCaption[index];
+        imgCaption.style.cssText = "display: none;";
     });
 });
 
