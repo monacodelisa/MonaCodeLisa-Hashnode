84b9f3ba01dda71ed852db318eacabf6fa0881da
Esther White <esteewhite@hotmail.com>
2023-01-24 08:41:54 +0200

    more images gallert projects

--- projects-widget/script.js
+++ projects-widget/script.js
@@ -93,29 +93,12 @@ dotsNav.addEventListener('click', e => {
 mainImgCaption.style.cssText = "display: none"
 
 mainImgWrapper.addEventListener('mouseover', () => {
-    mainImgCaption.style.cssText = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border-radius: 1rem; cursor: pointer;"
+    mainImgCaption.style.cssText = "position: absolute; top: 15px; left: 0; width: 100%; height: 87%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; border-radius: 1rem; cursor: pointer;"
 });
 
 mainImgWrapper.addEventListener('mouseleave', () => {
     mainImgCaption.style.cssText = "display: none"
 });
-
-// Main Image
-
-// mainImgWrapper.style.cssText = "width: 45vw; min-height: 55vh; margin: auto;position: relative;"
-
-// mainImg.style.cssText = "width: 100%; margin-bottom: 2rem; border-radius: 1rem;"
-
-// mainImgCaption.style.cssText = "display: none"
-
-// mainImgWrapper.addEventListener("mouseover", function() {
-//     mainImgCaption.style.cssText = "position: absolute; top: 20%; left: 0; width: 100%; height: 50%; overflow: hidden; background-color: #373737a8; color: white; font-size: 1.3rem; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer"
-// });
-
-// mainImgWrapper.addEventListener("mouseout", function() {
-//     mainImgCaption.style.cssText = "display: none" 
-// });
-
 
 // Gallery Grid 
 threeGridWrapper.forEach(wrapper => {
@@ -140,9 +123,4 @@ threeGridWrapper.forEach((wrapper, index) => {
         imgCaption.style.cssText = "display: none;";
     });
 });
-
-
-
-
-
 
